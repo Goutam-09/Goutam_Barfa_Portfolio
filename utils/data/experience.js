9e198849b34cbeb08c7a6f8ad8873d62b81211ea
Goutam-09 <141731357+Goutam-09@users.noreply.github.com>
2024-08-28 16:18:22 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,7 +3,12 @@ export const experiences = [
     id: 1,
     title: 'Full-Stack Developer',
     company: "Dollop Infotech Private Ltd.",
-    duration: "(April 2023 - Present)"
+    duration: "(June 2023 - July 2024)"
   },
- 
+  {
+    id: 2,
+    title: 'Java Developer Intern',
+    company: "Dollop Infotech Private Ltd.",
+    duration: "(Jan 2023 - March 2023)"
+  },
 ]
