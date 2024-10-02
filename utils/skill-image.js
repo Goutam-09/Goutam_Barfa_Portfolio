b89c2364775e1afd8756e42649caa59a54f00b5d
Goutam-09 <141731357+Goutam-09@users.noreply.github.com>
2024-10-02 22:04:40 +0530

    Update skill-image.js

--- utils/skill-image.js
+++ utils/skill-image.js
@@ -235,7 +235,7 @@ export const skillsImage = (skill) => {
       return postman;
     case 'jenkins':
       return jenkins;
-    case 'sonarQube':
+    case 'sonarqube':
       return sonarQube;
     default:
       break;
