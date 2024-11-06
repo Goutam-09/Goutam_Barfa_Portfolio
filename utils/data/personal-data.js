6f155ba398679de7ce4778966915e422e16bd9f8
Goutam-09 <141731357+Goutam-09@users.noreply.github.com>
2024-11-06 13:52:32 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -13,6 +13,6 @@ export const personalData = {
   stackOverflow: '',
   leetcode: "",
   devUsername: "",
-  resume: "https://drive.google.com/file/d/1omTq2BTeDxFaTGykLYIL7Y_EA7B20ccJ/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1LHGd6zoulI1fOuG5BUL-3fMd6JRe6yzh/view?usp=sharing",
   skills: ['Java SE', 'Java EE', 'MySQL','Spring Boot','Spring MVC', 'Spring Data JPA','Web Services', 'RestFul APIs', 'Microservices', 'Angular', 'Jenkins', 'SonarQube' ]
 }
